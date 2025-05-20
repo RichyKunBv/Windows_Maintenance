9af53e4689e8c16a165ee33122ded529716154b0
Ricardo Escamilla <81182181+RichyKunBv@users.noreply.github.com>
2025-05-20 17:47:31 -0600

    Update Mantenimiento.bat

--- Mantenimiento.bat
+++ Mantenimiento.bat
@@ -1,36 +1,3 @@
-::[Bat To Exe Converter]
-::
-::YAwzoRdxOk+EWAjk
-::fBw5plQjdCyDJGyX8VAjFAtVWAWWAE+1BaAR7ebv/Nawq0MZW/UsR6La07qaIe4S5EbhSZ8u2XRV1sIPA3s=
-::YAwzuBVtJxjWCl3EqQJgSA==
-::ZR4luwNxJguZRRnk
-::Yhs/ulQjdF+5
-::cxAkpRVqdFKZSDk=
-::cBs/ulQjdF+5
-::ZR41oxFsdFKZSDk=
-::eBoioBt6dFKZSDk=
-::cRo6pxp7LAbNWATEpCI=
-::egkzugNsPRvcWATEpCI=
-::dAsiuh18IRvcCxnZtBJQ
-::cRYluBh/LU+EWAnk
-::YxY4rhs+aU+IeA==
-::cxY6rQJ7JhzQF1fEqQJiZksaHWQ=
-::ZQ05rAF9IBncCkqN+0xwdVsGAlTMbQs=
-::ZQ05rAF9IAHYFVzEqQIdKRVASQqLMWK/CKETiA==
-::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
-::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
-::cRolqwZ3JBvQF1fEqQIYLRhcQ0SBM2X6B7gT+qinvIo=
-::dhA7uBVwLU+EWGut120TaChAWQCLM3ja
-::YQ03rBFzNR3SWATElA==
-::dhAmsQZ3MwfNWATExkszIAJ/WQqgKgs=
-::ZQ0/vhVqMQ3MEVWAtB9wSA==
-::Zg8zqx1/OA3MEVWAtB9wSA==
-::dhA7pRFwIByZRRnk
-::Zh4grVQjdCyDJGyX8VAjFAtVWAWWAE+1BaAR7ebv/Nawq0MZW/UsR6La07qaIe4S5EbhSbss2G5flsIBBR5Wah3laxcxyQ==
-::YB416Ek+ZW8=
-::
-::
-::978f952a14a936cc963da21a135fa983
 @echo off
 title Mantenimiento de Windows - RichyKunBv
 color 0A
