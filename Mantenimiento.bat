6eee0cddd9a34139ed77cc20642b5cb5a9a2aea1
Ricardo Escamilla <81182181+RichyKunBv@users.noreply.github.com>
2025-05-19 20:42:01 -0600

    Update Mantenimiento.bat
    papu update

--- Mantenimiento.bat
+++ Mantenimiento.bat
@@ -1,67 +1,122 @@
 @echo off
-:: WebOS Windows Refap v1.2.0
-Color 0a
-echo --- Iniciando protoculo ---
+title Mantenimiento de Windows - RichyKunBv
+color 0A
 
-echo Ejecutando SFC para pasar el rato...
+:MENU
+cls
+echo ============================================
+echo         MANTENIMIENTO DE WINDOWS
+echo ============================================
+echo.
+echo    Editor: RichyKunBv
+echo    Versión: 2.0
+echo    Página: https://github.com/RichyKunBv
+echo.
+echo 1. Revisión del sistema
+echo 2. Limpieza básica
+echo 3. Limpieza completa
+echo 4. Análisis completo
+echo 5. Salir
+echo.
+set /p opcion=Selecciona una opción [1-5]:
+
+if "%opcion%"=="1" goto REVISION
+if "%opcion%"=="2" goto LIMPIEZA_BASICA
+if "%opcion%"=="3" goto LIMPIEZA_COMPLETA
+if "%opcion%"=="4" goto ANALISIS_COMPLETO
+if "%opcion%"=="5" goto SALIR
+goto MENU
+
+:REVISION
+cls
+echo [1/4] Revisión del sistema: comprobando integridad...
 sfc /scannow
 
-echo Curando con medikit...
+echo Revisando imagen de Windows...
 DISM /Online /Cleanup-Image /CheckHealth
-
-echo Reparando ando...
 DISM /Online /Cleanup-Image /RestoreHealth
+pause
+goto MENU
 
-echo Defrag C y D mamaron...
+:LIMPIEZA_BASICA
+cls
+echo [2/4] Limpieza básica: optimizando discos y checando errores...
 defrag C: /O
 defrag D: /O
 
-Color 0c
-echo Escaneando C y D en busca de virus de xvideos...
 chkdsk C: /scan
 chkdsk D: /scan
+pause
+goto MENU
 
-echo Otra vez actívate Javascript...
+:LIMPIEZA_COMPLETA
+cls
+echo [3/4] Limpieza completa: temporales, red, firewall y bloatware...
+
+echo Restableciendo DNS y red...
 ipconfig /flushdns
 netsh int ip reset
 netsh winsock reset
 netsh advfirewall reset
 
-Color 09
-echo Si...
+echo Ejecutando Liberador de espacio...
 cleanmgr /sagerun:1
 
-echo Creando algo...
-reagentc /info
-reagentc /enable
-
-:: Crear punto de restauración
-echo Creando punto de restauracion por si se pudre...
-wmic.exe /Namespace:\\root\default Path SystemRestore Call CreateRestorePoint "Mantenimiento", 100, 7
-
-:: Limpiar archivos temporales
-echo Deleting el bloatware escondido...
+echo Borrando temporales...
 del /s /q %temp%\*.* 2>nul
 del /s /q C:\Windows\Temp\*.* 2>nul
 
-:: Revisar procesos sospechosos
-echo Mirando si hay algun rat por ahi...
-tasklist | findstr /I /C:"malware" /C:"virus"
+echo Creando punto de restauración...
+reagentc /info
+reagentc /enable
+wmic.exe /Namespace:\\root\default Path SystemRestore Call CreateRestorePoint "Mantenimiento Completo", 100, 7
 
-:: Optimizar inicio
-echo Revisando que chucha arranca con Windows...
+echo Verificando procesos y arranque...
+tasklist | findstr /I /C:"malware" /C:"virus"
 wmic startup get caption,command
 
-:: Verificar estado del disco
-echo Checando si el disco mamó...
+echo Comprobando estado físico del disco...
+wmic diskdrive get status
+pause
+goto MENU
+
+:ANALISIS_COMPLETO
+cls
+echo [4/4] Análisis completo: ejecutando TODO el mantenimiento...
+sfc /scannow
+DISM /Online /Cleanup-Image /CheckHealth
+DISM /Online /Cleanup-Image /RestoreHealth
+
+defrag C: /O
+defrag D: /O
+
+chkdsk C: /scan
+chkdsk D: /scan
+
+ipconfig /flushdns
+netsh int ip reset
+netsh winsock reset
+netsh advfirewall reset
+
+cleanmgr /sagerun:1
+
+del /s /q %temp%\*.* 2>nul
+del /s /q C:\Windows\Temp\*.* 2>nul
+
+reagentc /info
+reagentc /enable
+wmic.exe /Namespace:\\root\default Path SystemRestore Call CreateRestorePoint "Mantenimiento Total", 100, 7
+
+tasklist | findstr /I /C:"malware" /C:"virus"
+wmic startup get caption,command
 wmic diskdrive get status
 
-Color 03
-Color 06
-Color 07
-Color 04
-Color 01
-Color 07
-echo uy un gay!
-shutdown -p
+echo Mantenimiento completo finalizado.
+pause
+goto MENU
+
+:SALIR
+echo Saliendo del programa...
+start https://github.com/RichyKunBv
+timeout /t 2 >nul
 exit
